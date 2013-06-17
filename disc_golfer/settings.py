3e5b5dced74836442d2c5256ad45d12e5aa100c1
Sam Clarke <darwin.galap@gmail.com>
2013-06-17 16:56:30 -0700

    add South, initial migration

--- disc_golfer/settings.py
+++ disc_golfer/settings.py
@@ -133,6 +133,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'score_keeper',
+    'south',
 )
 
 ACCOUNT_ACTIVATION_DAYS = 7   # One-week activation window;
